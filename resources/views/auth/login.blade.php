a7b69f1c1070f0ff1ae44cbb4d37d1db957c7bdc
Tdrynn <tuderyan1936@gmail.com>
2025-12-01 14:33:34 +0800

    Responsive Login dan Register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,33 +3,40 @@
 @section('content')
     @include('layouts.navbar.navbar_back')
     <div class="row">
-        <div class="vh-100 text-white d-flex align-items-center justify-content-center HomePage1 object-fit-cover">
-            <div class="login-card text-center text-black">
-                <img src="{{ Vite::asset('resources/images/LogoCLR.png') }}" alt="Logo" width="60">
-                <h4 style="font-weight: 700;">Log in to your account</h4>
-                <p style="font-size: 14px; color: #555;">Your amazing camping will begin here</p>
-                <form>
-                    <div class="form-container mx-auto">
-                        <div class="form-floating mb-3 text-start">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
-                            <label for="floatingInput">Email address</label>
+        <div class="container">
+            <div class="row">
+                <div
+                    class="col-md-8 vh-100 text-white d-flex align-items-center justify-content-center HomePage1 object-fit-cover">
+                    <div class="login-card text-center text-black">
+                        <img src="{{ Vite::asset('resources/images/LogoCLR.png') }}" alt="Logo" width="60">
+                        <h4 class="fs-4 fw-bold">Log in to your account</h4>
+                        <p class="fs-6" style="color: #555;">Your amazing camping will begin here</p>
+                        <form action="">
+                            <div class="m-2">
+                                <div class="form-floating mb-3 text-start">
+                                    <input type="email" class="form-control" id="floatingInput"
+                                        placeholder="name@example.com" required>
+                                    <label for="floatingInput">Email address</label>
+                                </div>
+
+                                <div class="form-floating mb-3 text-start border border-success-subtle">
+                                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+                                        required>
+                                    <label for="floatingPassword">Password</label>
+                                </div>
+
+                                <button type="submit" class="btn btn-success btn-green w-100 my-2">Log In</button>
+                            </div>
+                        </form>
+
+                        <div class="text-center mt-1">
+                            <p>Don't have an account? <a href="{{ route('register') }}" class="text-primary">Create
+                                    account</a>
+                            </p>
                         </div>
-
-                        <div class="form-floating mb-3 text-start border border-success-subtle">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
-                            <label for="floatingPassword">Password</label>
-                        </div>
-
-                        <button type="submit" class="btn btn-success btn-green w-100 my-2">Log In</button>
                     </div>
-
-                    <div class="text-center mt-1">
-                        <p>Don't have an account? <a href="{{ route('register') }}" class="text-primary">Create account</a>
-                        </p>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
-    </div>
     </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,46 +3,35 @@
 @section('content')
     @include('layouts.navbar.navbar_back')
     <div class="row">
-        <div class="HomePage1 container min-vh-100 d-flex align-items-center justify-content-center">
+        <div class="container">
             <div class="row">
-                <div class="col-lg-4 col-md-6 col-11">
-
+                <div class="col-md-8 vh-100 d-flex align-items-center justify-content-center HomePage1 object-fit-cover">
                     <div class="login-card text-center">
                         <img src="{{ Vite::asset('resources/images/LogoCLR.png') }}" alt="Logo" width="60">
-                        <h4 class="fw-bold">Create a new account</h4>
-                        <p class="text-muted" style="font-size: 14px;">Your amazing camping will begin here</p>
+                        <h4 class="fs-4 fw-bold">Create New Account</h4>
+                        <p class="fs-6" style="color: #555;">Your amazing camping will begin here</p>
 
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
-                            <div class="form-container mx-auto">
-
+                            <div class="m-2">
                                 <div class="form-floating text-start mb-3">
                                     <input type="text" class="form-control" id="name" placeholder="User Name" required>
-                                    <label for="name">User Name</label>
+                                    <label for="name">Name</label>
                                 </div>
-
-                                <div class="form-floating text-start mb-3">
-                                    <input type="tel" class="form-control" id="phone" placeholder="Phone Number" required>
-                                    <label for="phone">Phone Number</label>
-                                </div>
-
                                 <div class="form-floating text-start mb-3">
                                     <input type="email" class="form-control" id="email" placeholder="email" required>
                                     <label for="email">Email Address</label>
                                 </div>
-
                                 <div class="form-floating text-start mb-3">
                                     <input type="password" class="form-control" id="password" placeholder="Password"
                                         required>
                                     <label for="password">Password</label>
                                 </div>
-
                                 <div class="form-floating text-start mb-2">
                                     <input type="password" class="form-control" id="confirmPassword"
                                         placeholder="Confirm Password" required>
                                     <label for="confirmPassword">Confirm Password</label>
                                 </div>
-
                                 <button type="submit" class="btn btn-success btn-green w-100 my-3 mt-4">
                                     Create Account
                                 </button>
@@ -55,7 +44,6 @@
                             </div>
                         </form>
                     </div>
-
                 </div>
             </div>
         </div>
